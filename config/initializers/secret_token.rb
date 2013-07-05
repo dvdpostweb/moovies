a6bc630e9819bbe81b4f7b8fa8d42a39c08f59f0
popac <aleksandar.popovic@codepop.biz>
2013-07-05 11:20:34 +0200

    projet initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moovies::Application.config.secret_token = 'a2e9d5a157256c600dff9d0ea1fbf10369ab51930ff7f361764d361d93578ce5baa3b011d7d68eac9d9f96a1f4f265ff7d7836c664f093178173a8a29fd5ec4b'
