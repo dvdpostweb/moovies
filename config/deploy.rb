e47df397e96800da0cbfb852b2141696f50b35c0
tiguss <gs@dvdpost.be>
2013-07-24 00:50:02 +0200

    t

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 require 'thinking_sphinx/capistrano'
-
+require './config/boot'
 set :stages, %w(staging production)
 set :default_stage, "staging"
 set :whenever_command, "bundle exec whenever"
