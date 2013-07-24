399196663df4a400c0677c7024eec2cefb04d2ba
guillaume <gs@dvdpost.be>
2013-07-24 11:34:57 +0200

    index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @body_id = 'home'
+    @new_svod = Product.limit 6
+    @new_tvod = Product.where('products_id >500').limit 6
   end
 end
