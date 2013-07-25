3b9a5d0133e690d3371400f7085e5276111676e9
popac <aleksandar.popovic@codepop.biz>
2013-07-25 16:32:04 +0200

    register free

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,11 @@
 class SearchController < ApplicationController
   def index
+    @body_id = 'products_index'
     @filter = view_context.get_current_filter({})
     new_params = session[:sexuality] == 0 ? params.merge(:per_page => 15, :country_id => session[:country_id], :hetero => 1) : params.merge(:per_page => 15, :country_id => session[:country_id])
     @products = Product.filter(@filter, new_params)
     @directors = params[:kind] == :normal ?  Director.search_clean(params[:search], params[:page]) : 0
     @actors = Actor.search_clean(params[:search], params[:kind], params[:page])
-    
+    @countries = ProductCountry.visible.order    
   end
 end
