1e9fa3ced380b9c057c99d31cced17f97ef5a5ce
guillaume <gs@dvdpost.be>
2013-08-07 11:28:22 +0200

    price page

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -2,8 +2,11 @@ class InfoController < ApplicationController
   def index
     @body_id = params[:page_name]
     if params[:page_name] == 'abo'
-      @discount_unlimited = Discount.find(1)
-      
+      @discount_svod = Discount.find(Moovies.discount["svod_#{I18n.locale}"])
+      @discount_kids = Discount.find(Moovies.discount["kid_#{I18n.locale}"])
+      @discount_adult = Discount.find(Moovies.discount["adult_#{I18n.locale}"])
+      @discount_classic_adult = Discount.find(Moovies.discount["classic_adult_#{I18n.locale}"])
+      @discount_all = Discount.find(Moovies.discount["all_#{I18n.locale}"])
     end
   end
 end
