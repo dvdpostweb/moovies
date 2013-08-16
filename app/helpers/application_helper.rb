eab2342d55f94dbb4dfd51290997fd9345659fb0
tiguss <gs@dvdpost.be>
2013-08-16 11:14:31 +0200

    change padding of trash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -140,10 +140,6 @@ module ApplicationHelper
   end
 
   def image_url(source)
-      abs_path = image_path(source)
-      unless abs_path =~ /^http/
-        abs_path = "#{request.protocol}#{request.host_with_port}#{abs_path}"
-      end
-     abs_path
-    end
+    URI.join(root_url, image_path(source))
+  end
 end
