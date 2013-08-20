042581e0153642fe8dbd18742756241e4e20623e
popac <aleksandar.popovic@codepop.biz>
2013-08-20 14:45:26 +0200

    change layout for promotion

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,7 +1,9 @@
 class PromotionsController < ApplicationController
   def show
+    
     @partial = params[:id]
     @partial += "_#{params[:format]}" if params[:format]
+    @body_id = @partial
   end
 
   def update
