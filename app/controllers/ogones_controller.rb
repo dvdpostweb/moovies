c52cc962678247b7fc4621135ef225290376142b
popac <aleksandar.popovic@codepop.biz>
2013-08-20 16:28:12 +0200

    modification about ogone

--- app/controllers/ogones_controller.rb
+++ app/controllers/ogones_controller.rb
@@ -46,7 +46,7 @@ class OgonesController < ApplicationController
       customer.update_attributes(:customers_abo => 1, :customers_registration_step => 100, :subscription_expiration_date => duration, :auto_stop => auto_stop, :customers_abo_discount_recurring_to_date => recurring)
       customer.abo_history(action, customer.next_abo_type_id)
       customer.abo_history(abo_action, customer.next_abo_type_id)
-      #@ogone.product(:products_quantity => @ogone.product.products_quantity - 1)
+      
       if customer.gender == 'm' 
         gender = t('mails.gender_male')
       else
