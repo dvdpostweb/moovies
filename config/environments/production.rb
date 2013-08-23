5374cd4468377ee6e81893ed166751da605c6984
popac <aleksandar.popovic@codepop.biz>
2013-08-23 17:42:09 +0200

    samsung code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Moovies::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.ui.datepicker-fr.js jquery.ui.datepicker-nl.js jquery.ui.datepicker-en.js login.css promotions.css)
+  config.assets.precompile += %w( jquery.ui.datepicker-fr.js jquery.ui.datepicker-nl.js jquery.ui.datepicker-en.js login.css promotions.css promotions.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
