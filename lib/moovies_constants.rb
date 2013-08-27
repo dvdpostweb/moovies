5ab781e5182f45323e6cf8885ba1f0fe5da6fccd
guillaume <gs@dvdpost.be>
2013-08-27 14:28:13 +0200

    extrait vod

--- lib/moovies_constants.rb
+++ lib/moovies_constants.rb
@@ -251,13 +251,13 @@ module Moovies
     def token_sample
       HashWithIndifferentAccess.new.merge({
         :normal => '521b50542e6cb0.10995196',
-        :adult => '51b6f9964888b1.78963380'
+        :adult => '521c9a5fb01455.20491543'
       })
     end
     def data_sample
       HashWithIndifferentAccess.new.merge({
         :normal => {:imdb_id =>1, :product_id => 129769},
-        :adult => {:imdb_id =>2, :product_id => 127276}
+        :adult => {:imdb_id =>2, :product_id => 130546}
       })
     end
     
