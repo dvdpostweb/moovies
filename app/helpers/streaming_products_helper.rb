241ea4cbbbe271cb0b0061d29b0c35ef0ae1bdf5
popac <aleksandar.popovic@codepop.biz>
2013-08-27 11:57:28 +0200

    customers page + vod list

--- app/helpers/streaming_products_helper.rb
+++ app/helpers/streaming_products_helper.rb
@@ -23,7 +23,6 @@ module StreamingProductsHelper
       $("#player").html("<object width='696' height='389'><param name='movie' value='http://#{Moovies.streaming_url}/StrobeMediaPlayback.swf'></param><param name='flashvars' value='src=http://#{Moovies.streaming_url}/#{token_name}_#{streaming.languages.by_language(:fr).first.short_alpha}_#{streaming.subtitles.count > 0 ? streaming.subtitles.by_language(:fr).first.short_alpha : 'non'}.f4m&loop=false&autoPlay=true&streamType=recorded&verbose=true&initialBufferTime=5&expandedBufferTime=30'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param><embed src='http://#{Moovies.streaming_url}/StrobeMediaPlayback.swf' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='696' height='389' flashvars='src=http://#{Moovies.streaming_url}/#{token_name}_#{streaming.languages.by_language(:fr).first.short_alpha}_#{streaming.subtitles.count > 0 ? streaming.subtitles.by_language(:fr).first.short_alpha : 'non'}.f4m&loop=false&autoPlay=true&streamType=recorded&verbose=true&initialBufferTime=5&expandedBufferTime=30'></embed></object>")
       script
     end
-  else
     javascript_tag script
   end
 
