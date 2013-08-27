3413a55f3d348d5a3dbd91d7f919667ac2e6ed26
tiguss <gs@dvdpost.be>
2013-08-27 22:34:18 +0200

    history

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -300,7 +300,7 @@ class Customer < ActiveRecord::Base
 
   def get_all_tokens(kind = nil, type = nil, page = 1)
     if type == :old
-      tokens.expired(48.hours.ago.localtime).select('distinct vod_wishlists.*').ordered_old.joins(:products).where(:products => {:products_type => Moovies.product_kinds[kind]}).paginate(:per_page => 20, :page => page)
+      tokens.expired(48.hours.ago.localtime).select('distinct tokens.*').ordered_old.joins(:products).where(:products => {:products_type => Moovies.product_kinds[kind]}).paginate(:per_page => 20, :page => page)
     else
       if !kind.nil?
         tokens.available(48.hours.ago.localtime, Time.now).select('distinct tokens.*').ordered.joins(:streaming_products, :products).where(:streaming_products => { :available => 1 }, :products => {:products_type => Moovies.product_kinds[kind]})
