d8664b359be4f6fb3f3e13205914c8b022fcae3c
guillaume <gs@dvdpost.be>
2013-08-29 14:58:18 +0200

    test ogone prod

--- app/controllers/customers/confirmations_controller.rb
+++ app/controllers/customers/confirmations_controller.rb
@@ -3,7 +3,6 @@ class Customers::ConfirmationsController < Devise::ConfirmationsController
   private
 
     def after_confirmation_path_for(resource_name, resource)
-      current_customer.update_column(:customers_registration_step, 31)
       step_path(:id => 'step2')
     end
 end
