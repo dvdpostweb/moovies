461bb8b462e1005e4856057a17473fdc2cbdf48a
guillaume <gs@dvdpost.be>
2013-09-19 14:10:20 +0200

    review change

--- app/models/review_rating.rb
+++ app/models/review_rating.rb
@@ -1,12 +1,18 @@
 class ReviewRating < ActiveRecord::Base
+  establish_connection "common_#{Rails.env}"
   self.table_name = :reviews_rating
 
   alias_attribute :value, :reviews_interesting
 
   belongs_to :review
   belongs_to :customer, :foreign_key => :customers_id
-  
-  scope :by_customer, lambda {|customer| where(:customers_id => customer.to_param)}
+  before_create :set_source
+  scope :by_customer, lambda {|customer| where(:customers_id => customer.to_param, :source => 'PLUSH')}
+
+  private
+  def set_source
+    self.source = 'PLUSH'
+  end
 end
 
   
