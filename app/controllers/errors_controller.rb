410b9977b498ecef6c253d2cbc9d63282441ae71
popac <aleksandar.popovic@codepop.biz>
2013-10-07 16:03:04 +0200

    change error page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,5 @@
 class ErrorsController < ApplicationController
-  skip_before_filter :authenticate_user! # if using Devise
+  skip_before_filter :authenticate_customer! # if using Devise
   def not_found
     @body_id = "i#{rand(5)+1}"
   end
