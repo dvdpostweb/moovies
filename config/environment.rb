01e38f5e287b130c51ea7c609fa902ff50475aad
guillaume <gs@dvdpost.be>
2013-10-30 14:53:59 +0100

    force routes

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Moovies::Application.initialize!
 
+RouteTranslator.config do |config|
+  config.force_locale = true
+end
