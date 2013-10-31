9c3b64d91387227cabd7396fc0c51bb575c1d4e5
popac <aleksandar.popovic@codepop.biz>
2013-10-31 15:58:06 +0100

    discount id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_after_registration(path = nil)
-    if current_customer && current_customer.step != 100 && params[:controller] != 'devise/sessions' && params[:controller] != 'payment_methods' && !(params[:controller] == 'info' && params[:page_name] == 'conditions') && !params[:controller] == 'promotions'
+    if current_customer && current_customer.step != 100 && params[:controller] != 'devise/sessions' && params[:controller] != 'customers/sessions' && params[:controller] != 'payment_methods' && !(params[:controller] == 'info' && params[:page_name] == 'conditions') && !params[:controller] == 'promotions'
       if current_customer.step.to_i == 31
         if (params['controller'] == 'steps' && params[:id] == 'step2') || (params[:controller] == 'customers' && params[:action] == 'update')
         else
