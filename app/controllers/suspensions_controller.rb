cec63c07d8c19873315e983545f41326893025a5
guillaume <gs@dvdpost.be>
2013-11-14 15:02:35 +0100

    suspensions system

---
+++ app/controllers/suspensions_controller.rb
@@ -0,0 +1,62 @@
+class SuspensionsController < ApplicationController
+  def new
+    @already_suspended = current_customer.suspended?
+    if @already_suspended
+      @expiration_holidays_date = expiration_holdays_date
+    else
+      if suspension_count_current_year >= 3
+        @too_many_suspensions = true
+      else
+        @too_many_suspensions = false
+      end
+    end
+    if request.xhr?
+      render :layout => false
+    end
+      
+  end
+
+  def create
+    if !current_customer.suspended? && suspension_count_current_year < 3
+      if params[:suspensions] && params[:suspensions][:duration] && params[:suspensions][:duration].to_i > 0
+        duration = params[:suspensions][:duration].to_i
+        current_customer.suspension_status = 1
+        current_customer.subscription_expiration_date = current_customer.subscription_expiration_date + duration.days
+        current_customer.save(:validate => false)
+        current_customer.suspensions.create(:status => 'HOLIDAYS', :date_added => Time.now, :date_end => duration.days.from_now, :last_modified => Time.now, :user_modified => 55)
+        @error = false
+      else
+        @error = true
+      end
+      if request.xhr?
+        if @error == false
+          render :layout => false
+        else  
+          render :layout => false, :status => false
+        end
+      end
+      
+    end
+  end
+
+  private
+  def notify(error, url)
+    Rails.logger.debug { "error #{error} url #{url}" }
+    begin
+      Airbrake.notify(:error_message => "suspension problem : #{to_param} error #{error} url #{url}", :backtrace => $@, :environment_name => ENV['RAILS_ENV'])
+    rescue => e
+      logger.error("suspension problem : #{to_param} error #{error} url #{url}")
+      logger.error(e.backtrace)
+    end
+  end
+
+  def expiration_holdays_date
+    if suspension = Suspension.holidays.find_all_by_customer_id(current_customer.to_param).last
+      suspension.date_end
+    end
+  end
+
+  def suspension_count_current_year
+    Suspension.holidays.last_year.find_all_by_customer_id(current_customer.to_param).count
+  end
+end
