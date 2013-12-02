891cf5005b04326464b1dd9bea61e77ae38e1207
guillaume <gs@dvdpost.be>
2013-12-02 15:56:15 +0100

    country translate

--- app/models/product_country.rb
+++ app/models/product_country.rb
@@ -3,11 +3,14 @@ class ProductCountry < ActiveRecord::Base
 
   self.primary_key = :countries_id
 
-  alias_attribute :name, :countries_name
 
   has_many :products, :foreign_key => :products_countries_id
 
   scope :visible, where(:inprod => 1)
   scope :order, :order => 'countries_name asc'
+
+  def name
+    self[I18n.locale]
+  end
   
 end
