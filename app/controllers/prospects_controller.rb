8283134cdc77a759bab61edbc68106775b3e37b2
popac <aleksandar.popovic@codepop.biz>
2014-02-06 14:51:00 +0100

    prospect

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -1,4 +1,14 @@
 class ProspectsController < ApplicationController
+  def new
+    @prospect = Prospect.new
+  end
+
   def create
+    prospect = Prospect.new(params[:prospect])
+    if prospect.save
+      render :text => "OK"
+    else
+      render :text => "KO"
+    end
   end
 end
--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,3 +1,10 @@
 class Prospect < ActiveRecord::Base
-  
+  attr_accessible :email, :newsletters_partners, :firstname, :lastname, :locale_id, :gender, :birth_at, :zip
+  before_create :set_default
+  validates :email, :presence => true, :uniqueness => true
+  validates :firstname, :lastname, :locale_id, :gender, :birth_at, :zip, :presence => true
+  private
+  def set_default
+    self.newsletters_partners = 0
+  end
 end
