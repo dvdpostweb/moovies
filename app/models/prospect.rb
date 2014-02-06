5793d72ce3f54411c53793ff3d01435853e35fb2
guillaume <gs@dvdpost.be>
2014-02-06 15:17:42 +0100

    webservice ptg co registration

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,8 +1,16 @@
 class Prospect < ActiveRecord::Base
-  attr_accessible :email, :newsletters_partners, :firstname, :lastname, :locale_id, :gender, :birth_at, :zip
+  attr_accessible :email, :newsletters_partners, :firstname, :lastname, :locale_id, :gender, :birth_at, :zip, :civ
+  attr_reader :civ
   before_create :set_default
   validates :email, :presence => true, :uniqueness => true
-  validates :firstname, :lastname, :locale_id, :gender, :birth_at, :zip, :presence => true
+  validates :firstname, :lastname, :locale_id, :gender, :presence => true
+  def civ=(data)
+    if data == 'Mme'
+      self.gender = 'F'
+    else
+      self.gender = 'M'
+    end
+  end
   private
   def set_default
     self.newsletters_partners = 0
