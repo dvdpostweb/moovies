78b3d0f34fe3c82ad68706007993a5057d678da3
popac <aleksandar.popovic@codepop.biz>
2014-04-07 13:55:59 +0200

    remove

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => 'AKIAICQS7KIVA5N62SKQ',
-  :secret_access_key => 'Au/ZyAC8yBAZGGSPdGDNEz00v2biQZPjUnxpd+qLl3Xn',
+  :secret_access_key => 'Au/ZyAC8yBAZGGSPdGDNEz00v2biQZPjUnxpd+qLl3Xn'
