b108611eff40b6210c1d848bb16c9e3efa347945
guillaume <gs@dvdpost.be>
2014-04-07 13:59:47 +0200

    change key

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,5 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => 'AKIAICQS7KIVA5N62SKQ',
-  :secret_access_key => 'Au/ZyAC8yBAZGGSPdGDNEz00v2biQZPjUnxpd+qLl3Xn'
+  :access_key_id     => 'AKIAI53MSJVZ7SH6VGCQ',
+  :secret_access_key => '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',
+  :server => "email-smtp.eu-west-1.amazonaws.com",
+  :port => 465
