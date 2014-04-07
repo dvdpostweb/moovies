19e65bf9fb2b729b0e9ee56a1193fb0f07114a1a
popac <aleksandar.popovic@codepop.biz>
2014-04-07 15:18:27 +0200

    remove ses gem

--- config/initializers/amazon_ses.rb
+++
@@ -1,5 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => 'AKIAI53MSJVZ7SH6VGCQ',
-  :secret_access_key => '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',
-  :server => "email-smtp.eu-west-1.amazonaws.com",
-  :port => 465
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {  
   :address              => "email-smtp.eu-west-1.amazonaws.com", 
   :port                 => 465,
-  :domain               => "staging.plush.be",
+  :domain               => "plush.be",
   :authentication       => "login",
   :user_name            => "AKIAICQS7KIVA5N62SKQ",
   :password             => "Au/ZyAC8yBAZGGSPdGDNEz00v2biQZPjUnxpd+qLl3Xn",
