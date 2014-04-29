d3fb560255bf172b381fba9b307cb112f508e43e
guillaume <gs@dvdpost.be>
2014-04-29 12:05:58 +0200

    remove lucky cycle

--- app/models/token.rb
+++ app/models/token.rb
@@ -6,7 +6,6 @@ class Token < ActiveRecord::Base
   has_many :streaming_products, :primary_key => :imdb_id, :foreign_key => :imdb_id
   has_many :streaming_products_free, :primary_key => :imdb_id, :foreign_key => :imdb_id
   has_many :token_ips
-  has_one :lucky_cycle
   has_many :products, :foreign_key => :imdb_id, :primary_key => :imdb_id
 
   after_create :generate_token
@@ -56,9 +55,6 @@ class Token < ActiveRecord::Base
           self.notify_error_token((customer ? customer.id : 0), Token.error[:query_rollback])
           return {:token => nil, :error => Token.error[:query_rollback]}
         else
-          if product.lucky_cycle?(nil, customer, file)
-            LuckyCycleAction.poke(file, I18n.locale, customer, token)
-          end
           return {:token => token, :error => nil}
         end
       else
