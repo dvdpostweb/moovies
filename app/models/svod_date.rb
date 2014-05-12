ee7d0c10282e2d196f214763dada28ce21d60a58
popac <aleksandar.popovic@codepop.biz>
2014-05-12 16:59:40 +0200

    free movies

--- app/models/svod_date.rb
+++ app/models/svod_date.rb
@@ -2,4 +2,6 @@ class SvodDate < ActiveRecord::Base
   scope :current, where('(start_on <= date(now()) and end_on >= date(now())) or (start_on > date(now()))')
   scope :svod, where('start_on <= date(now()) and end_on >= date(now())')
   scope :order, order("start_on asc")
+  scope :prepaid_svod, where('prepaid_start_on <= date(now()) and prepaid_end_on >= date(now()) and kind = "PREPAID_SVOD"') 
+  scope :prepaid_all, where('prepaid_start_on <= date(now()) and prepaid_end_on >= date(now()) and kind = "PREPAID_ALL"') 
 end
