6aa05fe1c25b145bfa8d4b6c9f1f963f21487494
guillaume <gs@dvdpost.be>
2014-05-14 11:01:12 +0200

    hash sha in

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -60,7 +60,7 @@ class PaymentMethodsController < ApplicationController
     @alias = "p#{current_customer.to_param}"
     OgoneCheck.create(:orderid => @order_id, :amount => (@price*100).to_i, :customers_id => current_customer.to_param, :context => internal_com, :site => 1, :language_id => Moovies.customer_languages[I18n.locale], :products_id => product_id)
     #list = {:COM => @com, :ALIAS => @alias, :AMOUNT => (@price*100).to_i, :CURRENCY => 'EUR', :LANGUAGE => @ogone_language, :ORDERID => @order_id, :PSPID => Moovies.ogone_pspid[Rails.env], :CN => current_customer.name, :ALIASUSAGE => @com, :DECLINEURL => @url_back, :EXCEPTIONURL => @url_back, :CANCELURL => @url_back, :CATALOGURL => @url_back, :ACCEPTURL => @url_ok, :TP => @template_ogone}
-    list = {:ALIAS => @alias, :AMOUNT => (@price*100).to_i, :CURRENCY => 'EUR', :LANGUAGE => @ogone_language, :ORDERID => @order_id, :PSPID => Moovies.ogone_pspid[Rails.env]}
+    list = {:AMOUNT => (@price*100).to_i, :CURRENCY => 'EUR', :LANGUAGE => @ogone_language, :ORDERID => @order_id, :PSPID => Moovies.ogone_pspid[Rails.env]}
     list = list.merge(:PM => 'CreditCard', :BRAND => @brand) if !@brand.nil?
     list = list.sort
     string = list.map { |k,v| "#{k.to_s.upcase}=#{v}#{Moovies.ogone_pass[Rails.env]}" }.join()
