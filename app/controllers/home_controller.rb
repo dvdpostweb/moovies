7a08ee0b19b84e8ac517679b8afcdad1787b7f01
guillaume <gs@dvdpost.be>
2014-06-03 16:20:53 +0200

    carousel public

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,9 @@ class HomeController < ApplicationController
       @carousel = params[:kind] == :adult ? @carousel.adult : @carousel.private
       @carousel = @carousel.order_rand.limit(1).first
       @vod_wishlist = current_customer.products.collect(&:products_id)
+    else
+      @carousel = Landing.by_language(I18n.locale).not_expirated
+      @carousel = params[:kind] == :adult ? @carousel.adult_public : @carousel.public
     end
   end
 
