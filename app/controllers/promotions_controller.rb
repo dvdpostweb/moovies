f8de89d9ead6a0cf528358bdae8b2770772fe609
guillaume <it@dvdpost.be>
2014-08-28 13:01:54 +0200

    promotions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -108,9 +108,9 @@ class PromotionsController < ApplicationController
                 redirect_to step_path(:id => 'step2')
               end
             elsif current_customer.abo_active == 1 && @activation && @activation.all_cust?
-              current_customer.tvod_free = current_customer.tvod_free + @promotion.tvod_free if @promotion && @promotion.tvod_free && @promotion.tvod_free > 0
+              current_customer.tvod_free = current_customer.tvod_free + @activation.tvod_free if @activation && @activation.tvod_free && @activation.tvod_free > 0
               current_customer.save(:validate => false)
-              current_customer.abo_history(38, current_customer.abo_type_id)
+              current_customer.abo_history(38, current_customer.abo_type_id, @activation.id)
               @activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
               redirect_to root_localize_path, notice: t('session.promotion.sucess') and return
             else
