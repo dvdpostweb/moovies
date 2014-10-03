94cec852815623d733a05270857818c4db0664ba
popac <aleksandar.popovic@codepop.biz>
2014-10-03 15:21:56 +0200

    home product

--- app/models/home_product.rb
+++ app/models/home_product.rb
@@ -1,3 +1,10 @@
 class HomeProduct < ActiveRecord::Base
   belongs_to :product
+  scope :kind, lambda {|kind| where(:kind => kind)}
+  scope :country, lambda {|country|  where(:country => country)}
+  scope :locale, lambda {|locale_id|  where(:locale_id => locale_id)}
+
+  scope :ordered, :order => 'id desc'
+  scope :limited, :limit => '8'
+
 end
