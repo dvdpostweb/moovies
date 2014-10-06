b8df92701b0a5f88181d3a113dfb1c15dc253bf7
guillaume <it@dvdpost.be>
2014-10-06 14:55:43 +0200

    add list creation

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController
+  def new
+  end
+
+  def create
+    10.times do |j|
+      i = 9 - j
+      if params["product_id_#{i}"] && !params["product_id_#{i}"].empty?
+        fr = params["fr_#{i}"] || 0
+        nl = params["nl_#{i}"] || 0
+        en = params["en_#{i}"] || 0
+
+        List.create(:product_id => params["product_id_#{i}"], :fr => fr, :nl => nl, :en => en)
+      end
+    end
+    redirect_to new_list_path
+  end
+end
