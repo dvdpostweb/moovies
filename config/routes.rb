38df6cfcfeaedebf7ff94c7693ab3b3e72fa4976
popac <aleksandar.popovic@codepop.biz>
2014-10-06 14:55:43 +0200

    add list creation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Moovies::Application.routes.draw do
   root :to => 'home#index'
   resource :ogone, :only => [:create]
   resource :home_products, :only => [:update, :edit]
+  resource :list, :only => [:new, :create]
+
   match 'flag' => 'home#flag'
   resources :prospects, :only => [:create, :new]
   scope '(:kind)', :kind => /normal|adult/ do
