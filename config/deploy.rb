b2a531b765e858875d1c1562fff5ceff64e090b9
guillaume <it@dvdpost.be>
2014-10-28 16:16:41 +0100

    add detail ps

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,16 +9,3 @@ set :whenever_command, "bundle exec whenever"
 after 'deploy:create_symlink' do
   run "ln -nfs /data/geoip/GeoIP.dat #{current_path}/GeoIP.dat"
 end
-
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
