be1850937134fbacb26ee62f2cf6e27d7ca9a0e2
popac <web.guru.serbia@gmail.com>
2016-03-23 21:11:57 +0100

    Set deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 load 'deploy/assets'
-set :whenever_environment, defer { stage }
-set :whenever_identifier, defer { "#{application}_#{stage}" }
+#set :whenever_environment, defer { stage }
+#set :whenever_identifier, defer { "#{application}_#{stage}" }
 set :whenever_command, "bundle exec whenever"
 
 require "whenever/capistrano"
