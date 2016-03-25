a9874e2996a7c316039a9f7cf924023b2a1b42e3
popac <web.guru.serbia@gmail.com>
2016-03-25 01:31:40 +0100

    Set Photobox Error

--- app/assets/javascripts/photobox.js
+++ app/assets/javascripts/photobox.js
@@ -1,16 +1,14 @@
 $( document ).ready(function() {
-
     if (window.location.pathname == "/en/photobox" || window.location.pathname == "/fr/photobox" || window.location.pathname == "/nl/photobox") {
     	$("#photobox-btn").click(function() {
-
 			if ($('#select-offer-2').is(':checked')) {
-			    window.location.href = gon.case2FILMS75
+		       window.location.href = gon.case2FILMS75
 	    	} else if ($('#select-offer-4').is(':checked')) {
 	    		window.location.href = gon.case4FILMS75
 	    	} else if ($('#select-offer-6').is(':checked')) {
-	    		window.location.href = gon.case6FILMS75
+	    		window.location.href = gon.case6FILMS7
 	    	} else {
-	    		alert("!");
+	    		$(".photobox-error").show().fadeOut(3000);
 	    	}
     	});
     }
