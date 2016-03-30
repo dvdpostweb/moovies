7547416f356e607ce0cab5ed48b577f88b141bfc
popac <web.guru.serbia@gmail.com>
2016-03-30 22:53:00 +0200

    Set html save option

--- app/controllers/freetrial_controller.rb
+++ app/controllers/freetrial_controller.rb
@@ -14,6 +14,22 @@ class FreetrialController < ApplicationController
   	gon.case6FILMSFREE = "mijn-account/sign_up?code=6FILMSFREE" if I18n.locale == :nl
   	gon.case6FILMSFREE = "my-account/sign_up?code=6FILMSFREE" if I18n.locale == :en
 
+    entxt if I18n.locale == :en
+    nltxt if I18n.locale == :nl
+    frtxt if I18n.locale == :fr
+
+  end
+
+  def entxt
+    @text = "Get one month free trial Choose the plan that best suits </br> you and rent 2, 4 o 6 movies for free for a month"
+  end
+
+  def nltxt
+    @text = "Geniet van een maand gratis abonnement Kies het pakket </br> dat het beste bij u past en huren 2, 4 of 6 films gratis voor 1 maand"
+  end
+
+  def frtxt
+    @text = "Profitez de Choisissez le forfait qui vous convient le mieux </br> et louez 2,4, ou 6 films gratuiement pendant 1 mois"
   end
 
 end
