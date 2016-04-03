5936e9398a643d5f5603ce0f36d25e8790b68e7f
popac <web.guru.serbia@gmail.com>
2016-04-03 22:35:48 +0200

    change order

--- app/controllers/public_promotions_controller.rb
+++ app/controllers/public_promotions_controller.rb
@@ -10,16 +10,16 @@ class PublicPromotionsController < ApplicationController
     activation = Activation.by_name(params[:promotion]).available.first
     if discount.present?
       render :text => new_customer_registration_path(:code => params[:promotion]);
+    elsif photobox_code.present? && params[:promotion] == "PHOTOBOX"
+      render :text => photobox_path(:code => params[:promotion]);
+    elsif freetrial_code.present? && params[:promotion] == "FREETRIAL"
+      render :text => freetrial_path(:code => params[:promotion]);
     elsif activation.present?
       if activation.abo_type_id == 0 #carrefour promotion
         render :text => carrefour_path(:carrefour_code => params[:promotion]);
       else
         render :text => new_customer_registration_path(:code => params[:promotion]);
       end
-    elsif photobox_code.present? && params[:promotion] == "PHOTOBOX"
-      render :text => photobox_path(:code => params[:promotion]);
-    elsif freetrial_code.present? && params[:promotion] == "FREETRIAL"
-      render :text => freetrial_path(:code => params[:promotion]);
     else
       respond_to do |format|
         format.html {render :text => t('public_promotion.update.error')}
