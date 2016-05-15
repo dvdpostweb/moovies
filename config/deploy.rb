673e763be5b77734cfb068c59fbac7ac34de98bc
popac <web.guru.serbia@gmail.com>
2016-05-15 21:14:16 +0200

    Set slack deploy webhook

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,37 @@ require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 require 'thinking_sphinx/capistrano'
 require './config/boot'
-require 'capistrano-slack-notify'
+require 'capistrano/slack'
 
 set :stages, %w(staging production)
 set :default_stage, "staging"
 set :whenever_command, "bundle exec whenever"
-set :slack_webhook_url,   "https://hooks.slack.com/services/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"
 
 
-before 'deploy', 'slack:starting'
-after  'deploy', 'slack:finished'
-before 'deploy:rollback', 'slack:failed'
+set :deployer do
+  name = `git config user.name`.strip
+  name = `whoami`.strip if name == ''
+  name
+end
+
+set :slack_token, 'https://hooks.slack.com/services/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' # comes from inbound webhook integration
+set :slack_room, 'general'
+set :slack_subdomain, 'dvdpost'
+set :slack_emoji, ':shipit:'
+set :slack_deploy_defaults, false # Provided tasks are weird, and hooks are quite absurd. Let's do it ourselves.
+
+namespace :slack do
+  task :starting do
+    slack_connect "#{fetch(:deployer)} *started* deploying to *#{fetch(:stage)}*. :rocket:"
+  end
+
+  task :finished do
+    slack_connect "#{fetch(:deployer)} *finished* deploying to *#{fetch(:stage)}*. :star2:"
+  end
+end
+
+before 'deploy:update_code', 'slack:starting'
+after 'deploy:restart', 'slack:finished'
 
 after 'deploy:create_symlink' do
   run "ln -nfs /data/geoip/GeoIP.dat #{current_path}/GeoIP.dat"
