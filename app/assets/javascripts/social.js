6be370b5d3824135ba20bdc3c833df8b7de2a03f
popac <web.guru.serbia@gmail.com>
2016-05-23 19:21:15 +0200

    Set code

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	//if (gon.current_customer.social_network_tag !== null && gon.current_customer.facebook_activation !== null && gon.current_customer.customers_registration_step !== null && window.location.pathname != "/" + gon.locale + "/freetrial") {
-	//	$("#top").hide();
-	//	$("#lang").hide();
-	//	$("#wrap").hide();
-	//	$("#footer").hide();
-	//	$("body").removeClass("normal private en not_reload");
-	//	window.location.href = window.location.origin + "/" + gon.locale + "/freetrial";
-	//}
+	if (gon.current_customer.social_network_tag !== null && gon.current_customer.facebook_activation !== null && gon.current_customer.customers_registration_step !== null && window.location.pathname != "/" + gon.locale + "/freetrial") {
+		$("#top").hide();
+		$("#lang").hide();
+		$("#wrap").hide();
+		$("#footer").hide();
+		$("body").removeClass("normal private en not_reload");
+		window.location.href = window.location.origin + "/" + gon.locale + "/freetrial";
+	}
 });
