5e328f9405be32a72155b966ca260a4965c41082
popac <web.guru.serbia@gmail.com>
2016-06-28 19:20:05 +0200

    Set activations codes for orange promotion

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -61,6 +61,37 @@ class PromotionsController < ApplicationController
       else
         render :show
       end
+    elsif params[:id] == "orange"
+      if params[:code].present?
+        activation = Activation.find_by_activation_code(params[:code])
+        code = Customer.find_by_activation_discount_code_id(activation.id)
+        if activation.present?
+          if customer_signed_in?
+            if code.present? || activation.activation_code_validto_date < Date.today
+              flash[:alert] = t('session.error_alreadyused_code')
+            else
+              customer = current_customer
+              customer.tvod_free = current_customer.tvod_free + activation.tvod_free if customer.tvod_only?
+              customer.activation_discount_code_id = activation.id
+              if customer.save!
+                current_customer.abo_history(38, current_customer.abo_type_id, activation.to_param)
+                activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
+                redirect_to root_localize_path, notice: t('session.promotion.sucess') and return
+              end
+            end
+          else
+            if code.present? || activation.activation_code_validto_date < Date.today
+              flash[:alert] = t(' session.error_alreadyused_code')
+            else
+              redirect_to customers_reactive_path(:code => params[:code])
+            end
+          end
+        else
+          flash[:alert] = t('session.error_code_not_present')
+        end
+      else
+        flash[:alert] = t('session.error_code_missing')
+      end
     else
       code = params[:code]
       @discount = Discount.by_name(code).available.first
