05886e635011086832a3641db842b6f8b2c7a243
popac <web.guru.serbia@gmail.com>
2016-06-29 10:10:57 +0200

    Set users finding

--- app/controllers/public_promotions_controller.rb
+++ app/controllers/public_promotions_controller.rb
@@ -10,25 +10,20 @@ class PublicPromotionsController < ApplicationController
 
       if params[:promotion].present?
         activation = Activation.find_by_activation_code(params[:promotion])
+        code = Customer.find_by_activation_discount_code_id(activation.id)
         if activation.present?
           if customer_signed_in?
-            if current_customer.activation_discount_code_id == params[:promotion] || activation.activation_code_validto_date < Date.today
+            if code.present? || activation.activation_code_validto_date < Date.today
               render :text => t('session.error_alreadyused_code')
             else
               customer = current_customer
-              customer.tvod_free = current_customer.tvod_free + activation.tvod_free if customer.tvod_only?
+              customer.tvod_free = current_customer.tvod_free + activation.tvod_free if current_customer.tvod_only?
               customer.activation_discount_code_id = activation.id
               if customer.save!
                 current_customer.abo_history(38, current_customer.abo_type_id, activation.to_param)
                 activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
                 render :text => root_localize_path, notice: t('session.promotion.sucess') and return
               end
-            end
-          else
-            if current_customer.activation_discount_code_id == params[:promotion] || activation.activation_code_validto_date < Date.today
-              render :text =>  t(' session.error_alreadyused_code')
-            else
-              render :text => customers_reactive_path(:code => params[:code])
             end
           end
         else
