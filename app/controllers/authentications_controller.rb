3772c01b8b757f1752d64d5fa1be60a913a7f41a
popac <web.guru.serbia@gmail.com>
2016-07-01 17:54:24 +0200

    Set auto code for facebook

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -21,14 +21,49 @@ class AuthenticationsController < ApplicationController
 	  	  new_auth.email = auth['extra']['raw_info']['email']
 	  	  if new_auth.save
             auth = Authentication.find_by_provider_and_uid_and_email(auth['provider'], auth['uid'], auth['extra']['raw_info']['email'])
+            if cookies[:code].present?
+              discount = Discount.find_by_discount_code(cookies[:code])
+		  	  activation = Activation.find_by_activation_code(cookies[:code])
+	  	  	  if discount.present?
+	  	  	    auth.customer.tvod_free = auth.customer.tvod_free + discount.tvod_free if auth.customer.abo_type_id == 6	
+	  	  	    DiscountUse.create(:discount_code_id => discount.id, :customer_id => auth.customer.to_param, :discount_use_date => Time.now)
+	  	  	  elsif activation.present?
+	  	  	    auth.customer.tvod_free = auth.customer.tvod_free + activation.tvod_free if auth.customer.abo_type_id == 6
+	  	  	    activation.update_attributes(:customers_id => auth.customer.to_param, :created_at => Time.now.localtime)
+	  	  	  end
+		  	  auth.customer.facebook_activation = 1
+		  	  auth.customer.step = 33
+		  	  if auth.customer.save(:validate => false)
+		  	  	sign_in_and_redirect(:customer, auth.customer)
+		  	  	flash[:notice] = t('session.promotion.sucess')
+		  	  end
+            end
 	        sign_in_and_redirect(:customer, auth.customer)
 	      end
 	  	else
+
 	  	  customer = Customer.new
 	      customer.apply_omniauth(auth)
-	      #customer.skip_confirmation!
-	      customer.customers_registration_step = 777
-	      customer.activation_discount_code_type = "D"
+
+	  	  if cookies[:code].present?
+	  	  	discount = Discount.find_by_discount_code(cookies[:code])
+	  	  	activation = Activation.find_by_activation_code(cookies[:code])
+
+	  	  	if discount.present?
+	  	  	  customer.tvod_free = customer.tvod_free + discount.tvod_free if customer.abo_type_id == 6	
+	  	  	  DiscountUse.create(:discount_code_id => discount.id, :customer_id => customer.to_param, :discount_use_date => Time.now)
+	  	  	elsif activation.present?
+	  	  	  customer.tvod_free = customer.tvod_free + activation.tvod_free if customer.abo_type_id == 6
+	  	  	  activation.update_attributes(:customers_id => customer.to_param, :created_at => Time.now.localtime)
+	  	  	end
+
+	  	  	customer.code = cookies[:code]
+          else
+          	discount = Discount.find_by_discount_code("2FILMSFREE")
+          	customer.code = "2FILMSFREE"
+	  	  end
+	  	  customer.facebook_activation = 1
+	  	  customer.step = 33
 	      if customer.save(:validate => false)
 	        flash[:notice] = t('.social.network.fbconnect.registration.new')
 	        sign_in_and_redirect(:customer, customer)
