1d573cc93732a964b1f37c14e9d71b70e0250b06
popac <web.guru.serbia@gmail.com>
2016-07-01 13:28:50 +0200

    Set Toast messages

--- app/assets/javascripts/payment_methods.js
+++ app/assets/javascripts/payment_methods.js
@@ -2,8 +2,9 @@ $(function() {
   $('#payment_methods').on('submit', function(){
     if($('input[name=brand]:checked', '#payment_methods').val() == undefined)
     {
-      alert($('#alert_cc').html())
-      return false
+      //alert($('#alert_cc').html())
+      //return false
+      console.log("asdasdasd");
     }
   })
 })
