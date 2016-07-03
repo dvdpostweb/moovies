2f212ebf039aaf72dd349e0a355a25424325e111
popac <web.guru.serbia@gmail.com>
2016-07-03 13:01:23 +0200

    Set carrefourbonus redirection path

--- app/controllers/carrefourbonus_controller.rb
+++ app/controllers/carrefourbonus_controller.rb
@@ -9,7 +9,7 @@ class CarrefourbonusController < ApplicationController
         @error_abo = true
       end
       if @error_code == false && @error_abo == false
-        redirect_to customers_reactive_path( :code => params[:abo]) and return 
+        redirect_to new_customer_session_path(:code => params[:abo]) and return 
       end
     end
   end
