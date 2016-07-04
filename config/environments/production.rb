6ed7a06bd20044aea9b570722da3e9e9f5274d7c
popac <web.guru.serbia@gmail.com>
2016-07-04 12:54:37 +0200

    Fix css precompile error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Moovies::Application.configure do
   config.action_dispatch.ip_spoofing_check = false
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery.ui.datepicker-fr.js jquery.ui.datepicker-nl.js jquery.ui.datepicker-en.js login.css promotions.css promotions.js errors.css md5.js viewrightplayer.js viewrightplayer_control.js)
+  config.assets.precompile += %w( jquery.ui.datepicker-fr.js jquery.ui.datepicker-nl.js jquery.ui.datepicker-en.js login.css promotions.css promotions.js errors.css md5.js viewrightplayer.js viewrightplayer_control.js adult.css alacarte.css unlimited.css)
   #config.assets.precompile += %w( photobox.css )
 
   # Disable delivery errors, bad email addresses will be ignored
@@ -67,7 +67,7 @@ Moovies::Application.configure do
   I18n.config.enforce_available_locales = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { 
+  config.action_mailer.default_url_options = {
     :host => "www.plush.be",
     :only_path => false
   }
