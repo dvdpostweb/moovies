269b5ca5231ac862b87d1db05d1ee7b5f62b5585
popac <web.guru.serbia@gmail.com>
2016-07-08 13:39:14 +0200

    Set facebook

--- app/controllers/api/v1/facebook_controller.rb
+++ app/controllers/api/v1/facebook_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::FacebookController < ApplicationController
 
 	def activation
-		if params[:code].present?
+		#if params[:code].present?
 			cookies[:code] = params[:code]
-			render json: { status: 9, message: cookies[:code] }
-		end
+			render json: { status: 9, message: "FACEBOOK" }
+		#end
 	end
 end
