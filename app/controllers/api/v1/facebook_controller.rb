aa9b2f639bad307c2f3182b33889599ec801a99d
popac <web.guru.serbia@gmail.com>
2016-07-13 12:03:34 +0200

    fb

--- app/controllers/api/v1/facebook_controller.rb
+++ app/controllers/api/v1/facebook_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::FacebookController < ApplicationController
-
-	def activation
-		#if params[:code].present?
-			cookies[:code] = params[:code]
-			render json: { status: 9, message: "FACEBOOK" }
-		#end
-	end
+  def activation
+    render json: { status: 9, message: "#{cookies[:code]}" }
+  end
 end
