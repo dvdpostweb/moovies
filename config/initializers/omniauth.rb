282f236ab04d895965c335cd4cd7de4735b60ff4
popac <web.guru.serbia@gmail.com>
2016-07-14 11:02:25 +0200

    Finish fb and fix css

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"], scope: 'email', info_fields: 'email, first_name, last_name, gender'
-
 end
