1c1b2d238bceae4436a4bbd95a9f89b5f35d6649
popac <web.guru.serbia@gmail.com>
2016-07-16 11:10:36 +0200

    Set fb

--- app/controllers/api/v1/validator_controller.rb
+++ app/controllers/api/v1/validator_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ValidatorController < ApplicationController
 
   def check_presence_of_customer_email
     if request.xhr?
-    email = Customer.find_by_email(params[:customer][:email])
+      email = Customer.find_by_email(params[:customer][:email])
       if email.present?
         render json: TRUE
       else
