ca6e59dfe8579a1cd5ad3d3e386499a0a5c8662f
popac <web.guru.serbia@gmail.com>
2016-07-25 21:35:04 +0200

    SET MANUAL SQL FOR SHIT RAILS OLD

--- app/controllers/api/v1/registration_controller.rb
+++ app/controllers/api/v1/registration_controller.rb
@@ -24,27 +24,65 @@ class Api::V1::RegistrationController < ApplicationController
           end
         end
       elsif !params[:activation].present? && params[:code].present?
-        discount = Discount.by_name(params[:code]).available.first
-        activation = Activation.where(:activation_code => params[:code]).first
-        customer.customers_registration_step = 100
-        customer.activation_discount_code_type = 'A'
-        customer.activation_discount_code_id = activation.activation_id
-        customer.customers_abo_type = activation.activation_products_id
-        customer.customers_next_abo_type = activation.next_abo_type
-        customer.group_id = activation.activation_group
-        customer.customers_next_discount_code = activation.next_discount
-        customer.tvod_free = user.tvod_free + r["tvod_free"]
-        if customer.save(validate: false)
-          sign_in :customer, customer
-          if discount.present?
-            DiscountUse.create(:discount_code_id => discount.id, :customer_id => current_customer.to_param, :discount_use_date => Time.now)
+
+        aqueryold = <<-SQL
+        SELECT activation_id, activation_products_id, next_abo_type, activation_group, next_discount, tvod_free
+        FROM activation_code
+        WHERE activation_code='#{code}'
+        SQL
+        aresold = ActiveRecord::Base.connection.exec_query(aqueryold)
+
+        dqueryold = <<-SQL
+        SELECT discount_code_id, listing_products_allowed, next_abo_type, group_id, tvod_free, goto_step
+        FROM discount_code
+        WHERE discount_code='#{code}'
+        SQL
+        dresold = ActiveRecord::Base.connection.exec_query(dqueryold)
+
+        if aresold.present?
+          aresold.each do |r|
+            user.customers_registration_step = 100
+            user.activation_discount_code_type = 'A'
+            user.activation_discount_code_id = r["activation_id"]
+            user.customers_abo_type = r["activation_products_id"]
+            user.customers_next_abo_type = r["next_abo_type"]
+            user.group_id = r["activation_group"]
+            user.customers_next_discount_code = r["next_discount"]
+            user.tvod_free = user.tvod_free + r["tvod_free"]
+            user.customers_abo = 1
+            if user.save(:validate => false)
+              activation = Activation.find_by_activation_code(code)
+              activation.update_attributes(:customers_id => user.to_param, :created_at => Time.now.localtime)
+              redirect_to_root_path = root_path
+              render json: { status: 9, message: redirect_to_root_path }
+            else
+              flash[:error] = "Error while creating a user account. Please try again."
+              redirect_to root_url
+            end
           end
-          if activation.present?
-            activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
-          end
-          redirect_to_root_path = root_path
-          render json: { status: 9, message: redirect_to_root_path }
         end
+
+        if dresold.present?
+          dresold.each do |r|
+            user.customers_registration_step = r["goto_step"]
+            user.activation_discount_code_type = 'D'
+            user.activation_discount_code_id = r["discount_code_id"]
+            user.customers_abo_type = r["listing_products_allowed"]
+            user.customers_next_abo_type = r["next_abo_type"]
+            user.group_id = r["group_id"]
+            user.tvod_free = user.tvod_free + r["tvod_free"]
+            user.customers_abo = 1
+            if user.save(:validate => false)
+              DiscountUse.create(:discount_code_id => r["discount_code_id"], :customer_id => user.to_param, :discount_use_date => Time.now)
+              redirect_to_root_path = root_path
+              render json: { status: 9, message: redirect_to_root_path }
+            else
+              flash[:error] = "Error while creating a user account. Please try again."
+              redirect_to root_url
+            end
+          end
+        end
+
       elsif params[:activation].present? && params[:code].present?
         discount = Discount.by_name(params[:code]).available.first
         activation = Activation.by_name(params[:activation]).available.first
