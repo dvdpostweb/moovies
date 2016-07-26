e2cea6071110a21a1f73ac4163b55be3354994f8
popac <web.guru.serbia@gmail.com>
2016-07-26 15:34:03 +0200

    Set redirections

--- app/controllers/api/v1/registration_controller.rb
+++ app/controllers/api/v1/registration_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::RegistrationController < ApplicationController
           customer.preselected_registration_moovie_id = product.to_param
           if customer.save(validate: false)
             sign_in :customer, customer
-            redirect_to_product_path = product_path(:id => product.to_param)
+            redirect_to_product_path = edit_customer_payment_methods_path(:customer_id => customer.to_param, :type => :credit_card_tvod, :product_id => product.to_param) #product_path(:id => product.to_param)
             render json: { status: 9, message: redirect_to_product_path }
           end
         end
