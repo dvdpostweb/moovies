230a998c66ffd59b80be4762b29a7b341c9a72ae
popac <web.guru.serbia@gmail.com>
2016-07-26 23:39:10 +0200

    Set bugs validations

--- app/models/activation.rb
+++ app/models/activation.rb
@@ -66,6 +66,7 @@ class Activation < ActiveRecord::Base
 
   scope :by_name, lambda { |name| where(:activation_code => name) }
   scope :available, lambda { where('(activation_code_validto_date > ? or activation_code_validto_date is null) and customers_id = 0', Time.now.to_s(:db)) }
+  scope :orange, :conditions=>['activation_group = 8 OR activation_group = 13']
 
   def tvod_only
     subscription_type && subscription_type.id == 6
