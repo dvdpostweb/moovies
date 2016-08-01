390cbede6d8e38e95169566bdaa492b367970f7d
popac <web.guru.serbia@gmail.com>
2016-08-01 10:15:51 +0200

    Exclude devise tracable module from production for now

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -70,7 +70,7 @@ class Customer < ActiveRecord::Base
   after_save :set_samsung
   before_save :get_code_from_samsung
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable#, :trackable
+         :recoverable, :rememberable, :trackable
 
   alias_attribute :abo_active, :customers_abo
   alias_attribute :last_name, :customers_lastname
