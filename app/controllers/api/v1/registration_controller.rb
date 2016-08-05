ddbee3acdc85d99deaec8257349b24590465d83a
popac <web.guru.serbia@gmail.com>
2016-08-05 17:43:32 +0200

    Set products popups

--- app/controllers/api/v1/registration_controller.rb
+++ app/controllers/api/v1/registration_controller.rb
@@ -15,13 +15,13 @@ class Api::V1::RegistrationController < ApplicationController
         product = Product.where(:products_id => params[:moovie_id]).first
         if product
           customer.step = 100
-          customer.customers_abo_type = 6
-          customer.customers_next_abo_type = 6
-          customer.customers_abo_validityto = nil
+          customer.customers_abo_type = 1
+          customer.customers_next_abo_type = 1
+          #customer.customers_abo_validityto = nil
           customer.preselected_registration_moovie_id = product.to_param
           if customer.save(validate: false)
             sign_in :customer, customer
-            redirect_to_product_path = edit_customer_payment_methods_path(:customer_id => customer.to_param, :type => :tvod, :product_id => product.id, :source => 0) #product_path(:id => product.to_param)
+            redirect_to_product_path = edit_customer_payment_methods_path(:customer_id => customer.to_param, :type => :credit_card_tvod, :product_id => product.id, :source => 0) #product_path(:id => product.to_param)
             render json: { status: 9, message: redirect_to_product_path }
           end
         end
@@ -52,7 +52,7 @@ class Api::V1::RegistrationController < ApplicationController
             customer.customers_next_discount_code = r["next_discount"]
             customer.tvod_free = r["tvod_free"]
             if customer.save(:validate => false)
-              sign_in :customer, customer 
+              sign_in :customer, customer
               activation = Activation.find_by_activation_code(params[:code])
               activation.update_attributes(:customers_id => customer.to_param, :created_at => Time.now.localtime)
               redirect_to_root_path = root_path
