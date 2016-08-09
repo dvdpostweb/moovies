d56fc7b5cec991f559511e7b1064e92a4f779964
popac <web.guru.serbia@gmail.com>
2016-08-09 19:33:36 +0200

    Popup finished

---
+++ app/controllers/renew_monthly_subscription_controller.rb
@@ -0,0 +1,21 @@
+class RenewMonthlySubscriptionController < ApplicationController
+
+  def renew_monthly_credits_for_a_la_carte
+    #if params[:product].present?
+      product_abo = ProductAbo.find_by_products_id(current_customer.customers_next_abo_type)
+      customer = current_customer
+      customer.tvod_free = current_customer.tvod_free + product_abo.tvod_credits
+      customer.customers_abo_validityto = Time.now
+      customer.customers_locked__for_reconduction = 1
+      customer.credits_already_recieved = 1
+      if customer.save(validate: false)
+        if params[:product].present?
+          redirect_to product_path(:id => params[:product])
+        else
+          redirect_to root_path
+        end
+      end
+    #end
+  end
+
+end
---
+++ app/models/product_abo.rb
@@ -0,0 +1,4 @@
+class ProductAbo < ActiveRecord::Base
+  self.table_name = :products_abo
+  self.primary_key = :products_id
+end
