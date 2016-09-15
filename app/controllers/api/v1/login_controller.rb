71edfa477ffab906d8312eefcf2f4180d0e52994
popac <web.guru.serbia@gmail.com>
2016-09-15 02:24:20 +0200

    Set cookie identificator

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -65,6 +65,7 @@ class Api::V1::LoginController < ApplicationController
         if DiscountUse.create(:discount_code_id => discount.id, :customer_id => resource.to_param, :discount_use_date => Time.now)
           if resource.valid_password?(password)
             sign_in :customer, resource
+            cookies[:customer_identificator] = "#{current_customer.encrypted_password}_customer_identificator"
             if activation
               activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
             end
@@ -80,6 +81,7 @@ class Api::V1::LoginController < ApplicationController
     if resource.valid_password?(password)
       if moovie_id.present?
         sign_in :customer, resource
+        cookies[:customer_identificator] = "#{current_customer.encrypted_password}_customer_identificator"
         customer = current_customer
         customer.preselected_registration_moovie_id = moovie_id
         if customer.save(validate: false)
@@ -109,6 +111,7 @@ class Api::V1::LoginController < ApplicationController
         end
       else
         sign_in :customer, resource
+        cookies[:customer_identificator] = "#{current_customer.encrypted_password}_customer_identificator"
         redirect_to_root_localize_path = root_localize_path
         render json: { status: 1, message: redirect_to_root_localize_path }
       end
