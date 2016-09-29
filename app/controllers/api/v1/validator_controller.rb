2206c9f0ca410b5e6541b331a6de3bf932e857eb
popac <web.guru.serbia@gmail.com>
2016-09-29 21:50:59 +0200

    Visitor part finished

--- app/controllers/api/v1/validator_controller.rb
+++ app/controllers/api/v1/validator_controller.rb
@@ -80,33 +80,55 @@ class Api::V1::ValidatorController < ApplicationController
   end
 
   def check_and_validate_public_promotions_activation_codes
-    if request.xhr?
-      if params[:promotion].present?
-        activation = Activation.where(:activation_code => params[:promotion]).where(:customers_id => 0).orange.first
-        if !activation.present?
-          careefour = Activation.where(:activation_code => params[:promotion]).where(:customers_id => 0).where(:activation_group => 21).first
-          if careefour.present?
-            render :json => { :status => 2, :message => carrefour_path(:carrefour_activation_code => params[:promotion]) }
-          else
-            render :json => { :status => 1, :message => t('session.error_alreadyused_code') }
+    if customer_signed_in?
+      if request.xhr?
+        if params[:promotion].present?
+          activation = Activation.where(:activation_code => params[:promotion]).where(:customers_id => 0).orange.first
+          if !activation.present?
+            careefour = Activation.where(:activation_code => params[:promotion]).where(:customers_id => 0).where(:activation_group => 21).first
+            if careefour.present?
+              render :json => { :status => 2, :message => carrefour_path(:carrefour_activation_code => params[:promotion]) }
+            else
+              render :json => { :status => 1, :message => t('session.error_alreadyused_code') }
+            end
+          elsif activation.present?
+            customer = current_customer
+            customer.tvod_free = current_customer.tvod_free + activation.tvod_free
+            customer.code = params[:promotion]
+            customer.step = 100
+            customer.customers_abo = 1
+            if customer.save!
+              current_customer.abo_history(38, current_customer.abo_type_id, activation.to_param)
+              activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
+              render :json => { :status => 2, :message => root_localize_path }
+            end
           end
-        elsif activation.present?
-          customer = current_customer
-          customer.tvod_free = current_customer.tvod_free + activation.tvod_free
-          customer.code = params[:promotion]
-          customer.step = 100
-          customer.customers_abo = 1
-          if customer.save!
-            current_customer.abo_history(38, current_customer.abo_type_id, activation.to_param)
-            activation.update_attributes(:customers_id => current_customer.to_param, :created_at => Time.now.localtime)
-            render :json => { :status => 2, :message => root_localize_path }
-          end
+        elsif !params[:promotion].present?
+          render :json => { :status => 1, :message => t('session.error_code_missing') }
         end
-      elsif !params[:promotion].present?
-        render :json => { :status => 1, :message => t('session.error_code_missing') }
+      else
+        raise ActionController::RoutingError.new('Not Found')
       end
     else
-      raise ActionController::RoutingError.new('Not Found')
+      discount = Discount.by_name(params[:promotion]).available.first
+      activation = Activation.by_name(params[:promotion]).available.first
+      if params[:promotion] === "PHOTOBOX"
+        render :json => { :status => 2, :message => photobox_path(:code => params[:promotion]) }
+      elsif params[:promotion] === "FREETRIAL"
+        render :json => { :status => 2, :message => info_path(:page_name => "freetrial") }
+      elsif params[:promotion] === "CARREFOUR"
+        render :json => { :status => 2, :message => carrefourbonus_path(:code => params[:promotion]) }
+      elsif discount.present?
+        render :text => new_customer_session_path(:code => params[:promotion]);
+      elsif activation.present?
+        if activation.abo_type_id == 0 
+          render :json => { :status => 2, :message => carrefour_path(:carrefour_code => params[:promotion]) }
+        else
+          render :json => { :status => 2, :message => new_customer_session_path(:code => params[:promotion]) }
+        end
+      else
+        render :json => { :status => 1, :message => t('public_promotion.update.error') }
+      end
     end
   end
 
