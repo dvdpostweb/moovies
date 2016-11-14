3c594c692def967332510d271f657eea3719037d
popac <web.guru.serbia@gmail.com>
2016-11-14 14:24:20 +0100

    like damir 5

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -103,7 +103,7 @@ class PaymentMethodsController < ApplicationController
     @alias = "p#{current_customer.to_param}"
 
     if current_customer.have_freetrial_codes?
-      list = { "Account.PSPID" => ENV["OGONE_PSPID"], "Alias.AliasId" => @alias, "Alias.OrderId" => @order_id, "Alias.StorePermanently" => "Y", "Parameters.AcceptUrl" => "http://staging.plush.be/ogone", "Parameters.ExceptionUrl" => @url_back, "Card.Brand" => @brand.upcase, "Card.PaymentMethod" => "" } 
+      list = { "Account.PSPID" => ENV["OGONE_PSPID"], "Alias.AliasId" => @alias, "Alias.OrderId" => @order_id, "Alias.StorePermanently" => "Y", "Parameters.AcceptUrl" => "https://staging.plush.be/ogone", "Parameters.ExceptionUrl" => @url_back, "Card.Brand" => @brand.upcase, "Card.PaymentMethod" => "" } 
       #list = list.merge("Card.PaymentMethod" => @pm, "Card.Brand" => @brand.upcase) if !@brand.nil?
       list = list.sort
       s = list.map { |k,v| "#{k.to_s.upcase}=#{v}#{ENV["OGONE_PASSWORD"]}" }.join()
