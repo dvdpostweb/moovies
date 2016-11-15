bd1d87d4c9176fc37bd7fda1bca496463bc25af4
popac <web.guru.serbia@gmail.com>
2016-11-15 13:40:24 +0100

    INSPECT ALL PARAMS

--- app/controllers/api/v1/ogone_tokenization_controller.rb
+++ app/controllers/api/v1/ogone_tokenization_controller.rb
@@ -2,26 +2,22 @@ class Api::V1::OgoneTokenizationController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
   skip_before_filter :authenticate_customer!
-  protect_from_forgery except: [:ogone_parameters_accepturl, :ogone_parameters_exceptionurl]
+  protect_from_forgery except: [:ogone_parameters_accepturl]
 
   def ogone_parameters_accepturl
     if params[:Alias_AliasId].present?
-      customer = Customer.find(params[:Alias_AliasId].sub('p', ''))
-      if customer.present?
-        customer.step = 100
-        customer.customers_abo = 1
-        customer.customers_abo_validityto = Time.now + 1.month
-        customer.customers_abo_payment_method = 1
-        if customer.save(validate: false)
-          redirect_to step_path(:id => 'step4')
-        end
-      end
+      render json: params
+#      customer = Customer.find(params[:Alias_AliasId].sub('p', ''))
+#      if customer.present?
+#        customer.step = 100
+#        customer.customers_abo = 1
+#        customer.customers_abo_validityto = Time.now + 1.month
+#        customer.customers_abo_payment_method = 1
+#        if customer.save(validate: false)
+#          redirect_to step_path(:id => 'step4')
+#        end
+#      end
     end
-  end
-
-  def ogone_parameters_exceptionurl
-    locale = customer.locale || :fr
-    redirect_to step_path(:id => 'step3', :locale => locale)
   end
 
 end
