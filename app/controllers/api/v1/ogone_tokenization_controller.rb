9065d5612fcc2e38cd5e1d147023455e2e32f55b
popac <web.guru.serbia@gmail.com>
2016-11-15 12:58:23 +0100

    Find customer by trimed param

--- app/controllers/api/v1/ogone_tokenization_controller.rb
+++ app/controllers/api/v1/ogone_tokenization_controller.rb
@@ -6,8 +6,10 @@ class Api::V1::OgoneTokenizationController < ApplicationController
 
   def ogone_parameters_accepturl
     if params[:Alias_AliasId].present?
-      render json: params[:Alias_AliasId].sub('p', '')
-      
+      customer = Customer_find(params[:Alias_AliasId].sub('p', ''))
+      if customer.present?
+        render json:customer
+      end
     end
   end
 
