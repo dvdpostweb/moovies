344e51c2984fabb0b5ce5cb5f0155834e2948e04
popac <web.guru.serbia@gmail.com>
2016-11-15 13:52:31 +0100

    Finish ALL

--- app/controllers/api/v1/ogone_tokenization_controller.rb
+++ app/controllers/api/v1/ogone_tokenization_controller.rb
@@ -6,17 +6,22 @@ class Api::V1::OgoneTokenizationController < ApplicationController
 
   def ogone_parameters_accepturl
     if params[:Alias_AliasId].present?
-      render json: params
-#      customer = Customer.find(params[:Alias_AliasId].sub('p', ''))
-#      if customer.present?
-#        customer.step = 100
-#        customer.customers_abo = 1
-#        customer.customers_abo_validityto = Time.now + 1.month
-#        customer.customers_abo_payment_method = 1
-#        if customer.save(validate: false)
-#          redirect_to step_path(:id => 'step4')
-#        end
-#      end
+      customer = Customer.find(params[:Alias_AliasId].sub('p', ''))
+      if customer.present?
+        customer.step = 100
+        customer.customers_abo = 1
+        customer.customers_abo_validityto = Time.now + 1.month
+        customer.customers_abo_payment_method = 1
+        customer.ogone_card_type = params[:Card_Brand] if params[:Card_Brand].present?
+        customer.ogone_card_no = params[:Card_CardNumber] if params[:Card_CardNumber].present?
+        customer.ogone_exp_date = params[:Card_ExpiryDate] if params[:Card_ExpiryDate].present?
+        customer.ogone_owner = params[:Card_CardHolderName] if params[:Card_CardHolderName].present?
+        if customer.save(validate: false)
+          if customer.abo_history(17, customer.abo_type_id, "FREE")
+            redirect_to step_path(:id => 'step4')
+          end
+        end
+      end
     end
   end
 
