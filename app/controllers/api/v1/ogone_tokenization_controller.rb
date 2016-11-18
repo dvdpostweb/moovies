61d5db55b455073ceb6ba071b5864df5ead4d654
popac <web.guru.serbia@gmail.com>
2016-11-18 19:39:27 +0100

    Set locale

--- app/controllers/api/v1/ogone_tokenization_controller.rb
+++ app/controllers/api/v1/ogone_tokenization_controller.rb
@@ -18,7 +18,13 @@ class Api::V1::OgoneTokenizationController < ApplicationController
         customer.ogone_owner = params[:Card_CardHolderName] if params[:Card_CardHolderName].present?
         if customer.save(validate: false)
           if customer.abo_history(17, customer.abo_type_id, "FREE")
-            redirect_to step_path(:id => 'step4', :locale => cookies[:locale])
+            if  cookies[:locale] == "fr"
+              redirect_to step_path(:id => 'step4', :locale => "fr")
+            elsif cookies[:locale] == "nl"
+              redirect_to redirect_to step_path(:id => 'step4', :locale => "nl")
+            elsif cookies[:locale] == "en"
+              redirect_to redirect_to step_path(:id => 'step4', :locale => "en")
+            end
           end
         end
       end
