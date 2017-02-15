e2f9e2fd6246e5846958674b07bb2984cd79b3e7
popac <web.guru.serbia@gmail.com>
2017-02-15 16:00:41 +0100

    Set orange oauth try 1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
+require 'orange_oauth2'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], scope: 'email', info_fields: 'email, first_name, last_name, gender'
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
   provider :windowslive, ENV['WINDOWSLIVE_CLIENT_ID'], ENV['WINDOWSLIVE_SECRET'], :scope => 'wl.basic'
+  provider :orange, ENV['ORANGE_CILENT_ID'], ENV['ORANGE_CILENT_SECRET']#, :scope => 'openid'
 end
