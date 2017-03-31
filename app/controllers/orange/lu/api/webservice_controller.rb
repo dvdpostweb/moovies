20ddb2d281a4e5229c0440b5b985faa9773dbebc
Aleksandar Popovic <aleksandar.popovic@codepop.biz>
2017-03-31 20:52:45 +0200

    Set webservice api calls

--- app/controllers/orange/lu/api/webservice_controller.rb
+++ app/controllers/orange/lu/api/webservice_controller.rb
@@ -1,11 +1,11 @@
 class Orange::Lu::Api::WebserviceController < ApplicationController
 
   def is_eligable
-    render json: "is_eligable"
+    render json: HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangeIseligable?customers_id=0&mobileNumber=32642013990&SMSCodeMessage=%22dsgfdag%22&products_id=1758468")
   end
 
   def orange_purchase
-    render json: "orange_purchase"
+    render json: HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangePurchase?customers_id=0&mobileNumber=32642013990&price=4.99&products_id=1")
   end
 
 end
