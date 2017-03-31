4af411fb25eeb43f0d5bd64f8e4a9dd9239d6ef9
Aleksandar Popovic <aleksandar.popovic@codepop.biz>
2017-03-31 23:44:45 +0200

    Set api communication try forms

--- app/assets/javascripts/orange/lu/auth/sms.js
+++ app/assets/javascripts/orange/lu/auth/sms.js
@@ -2,15 +2,22 @@ $(document).ready(function () {
 
     if ((window.location.pathname == "/orange/lu/auth/" + gon.locale + "/sms/authorization")) {
         isEligable();
+        orangePurchase();
     }
 
 });
 
 function isEligable() {
+
     $("#is_eligable").validate({
         rules: {
             "phone-number": {
                 required: true
+            }
+        },
+        messages: {
+            "phone-number": {
+                required: isEligableValidationMessage()
             }
         },
         highlight: function(element) {
@@ -20,6 +27,140 @@ function isEligable() {
             $(element).closest('.form-group').removeClass('has-error');
         },
         errorElement: 'span',
-        errorClass: 'help-block'
+        errorClass: 'help-block',
+        submitHandler: function (form) {
+            $.ajax({
+                method: 'POST',
+                url: '/orange/lu/api/is_eligable',
+                data: {
+                    'sms_number': $.trim($("#sms_number").val())//,
+                    //'password': $("#customer_password").val(),
+                    //'code': gon.code,
+                    //'moovie_id': gon.moovie_id,
+                    //'activation': gon.activation,
+                    //'samsung': gon.samsung,
+                    //'kind': gon.kind
+                },
+                dataType: 'json',
+                success: function (response) {
+                    if (0 === response.status) {
+                        $("#info").html(successMessage(response.message)).fadeOut(3000);
+                        $("#is_eligable").hide();
+                        $("#orange_purchase").show();
+                    } else if (1 === response.status) {
+                        $("#info").append(errorMessage(response.message));
+                    //    window.location.href = response.message
+                    //} else if (3 === response.status) {
+                    //    $("#code_info").show().fadeOut(3000);
+                    //    $("#code_message").text("");
+                    //    $("#code_message").append(response.message);
+                    //} else if (5 === response.status) {
+                    //    $("#code_info").show().fadeOut(3000);
+                    //    $("#code_message").text("");
+                    //    $("#code_message").append(response.message);
+                    //} else if (4 === response.status) {
+                    //    window.location.href = response.message
+                    }
+                },
+                error: function (response) {
+                    console.log('CHECKED AJAX ERROR!!!');
+                }
+            });
+        }
     });
+}
+
+function orangePurchase() {
+
+    $("#orange_purchase").validate({
+        rules: {
+            "sms-code": {
+                required: true
+            }
+        },
+        messages: {
+            "sms-code": {
+                required: orangePurchaseMessage()
+            }
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        submitHandler: function (form) {
+            $.ajax({
+                method: 'POST',
+                url: '/orange/lu/api/orange_purchase',
+                data: {
+                    'sms_code': $.trim($("#sms_code").val())//,
+                    //'password': $("#customer_password").val(),
+                    //'code': gon.code,
+                    //'moovie_id': gon.moovie_id,
+                    //'activation': gon.activation,
+                    //'samsung': gon.samsung,
+                    //'kind': gon.kind
+                },
+                dataType: 'json',
+                success: function (response) {
+                    if (0 === response.status) {
+                        $("#info").html(successMessage(response.message)).fadeOut(3000);
+                        //$("#sms_form").empty();//.html(orangePurchaseForm());
+                    } else if (1 === response.status) {
+                        $("#info").append(errorMessage(response.message));
+                        //    window.location.href = response.message
+                        //} else if (3 === response.status) {
+                        //    $("#code_info").show().fadeOut(3000);
+                        //    $("#code_message").text("");
+                        //    $("#code_message").append(response.message);
+                        //} else if (5 === response.status) {
+                        //    $("#code_info").show().fadeOut(3000);
+                        //    $("#code_message").text("");
+                        //    $("#code_message").append(response.message);
+                        //} else if (4 === response.status) {
+                        //    window.location.href = response.message
+                    }
+                },
+                error: function (response) {
+                    console.log('CHECKED AJAX ERROR!!!');
+                }
+            });
+        }
+    });
+
+}
+
+function isEligableValidationMessage() {
+    if (gon.locale == "fr") {
+        return "Numéro de téléphone est nécessaire."
+    } else if (gon.locale == "nl") {
+        return "Telefoonnummer is vereist."
+    } else if (gon.locale == "en") {
+        return "Phone number is required."
+    }
+}
+
+function orangePurchaseMessage() {
+    if (gon.locale == "fr") {
+        return "Numéro de téléphone est nécessaire."
+    } else if (gon.locale == "nl") {
+        return "Telefoonnummer is vereist."
+    } else if (gon.locale == "en") {
+        return "Phone number is required."
+    }
+}
+
+function successMessage(message) {
+    return successMessage = "<div class=\"alert alert-success\">" +
+        "<strong>" + message + "</strong>" +
+        "</div>";
+}
+
+function errorMessage(message) {
+    return errorMessage = "<div class=\"alert alert-danger\">" +
+        "<strong> + message + </strong>" +
+        "</div>";
 }
--- app/controllers/orange/lu/api/webservice_controller.rb
+++ app/controllers/orange/lu/api/webservice_controller.rb
@@ -1,11 +1,32 @@
 class Orange::Lu::Api::WebserviceController < ApplicationController
+  require 'json'
 
   def is_eligable
-    render json: HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangeIseligable?customers_id=0&mobileNumber=32642013990&SMSCodeMessage=%22dsgfdag%22&products_id=1758468")
+    if request.xhr? && URI(request.referer).path == "/orange/lu/auth/#{I18n.locale}/sms/authorization" && params[:sms_number].present?
+      customers_id = customer_signed_in? ? current_customer : 0
+      wcf_service_response = HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangeIseligable?customers_id=#{customers_id}&mobileNumber=#{params[:sms_number]}&SMSCodeMessage=%22dsgfdag%22&products_id=1758468")
+      if wcf_service_response.body = 0
+        render json: { status: 0, message: "Success message that SMS CODE IS SENT" }
+      elsif wcf_service_response.body == 1
+        render json: { status: 1, message: "Error" }
+      end
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
   def orange_purchase
-    render json: HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangePurchase?customers_id=0&mobileNumber=32642013990&price=4.99&products_id=1")
+    if request.xhr? && URI(request.referer).path == "/orange/lu/auth/#{I18n.locale}/sms/authorization" && params[:sms_code].present?
+      customers_id = customer_signed_in? ? current_customer : 0
+      wcf_service_response = HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangePurchase?customers_id=#{customers_id}&mobileNumber=#{params[:sms_code]}&price=4.99&products_id=1")
+      if wcf_service_response.body = 0
+        render json: { status: 0, message: "Success message that SMS CODE IS VALID" }
+      elsif wcf_service_response.body == 1
+        render json: { status: 1, message: "Error" }
+      end
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
 end
