f79309a113eb9926682d30297113cc3dfbf0e046
Aleksandar Popovic <aleksandar.popovic@codepop.biz>
2017-04-02 21:11:05 +0200

    Set alacarte code packages to gon variables

--- app/controllers/orange/lu/auth/sms_controller.rb
+++ app/controllers/orange/lu/auth/sms_controller.rb
@@ -2,11 +2,17 @@ class Orange::Lu::Auth::SmsController < ApplicationController
 
   def authorization
     if params[:code].present? && params[:code] == "2FILMS"
-      gon.product_id = 2
+      gon.products_id = 7
     elsif params[:code].present? && params[:code] == "4FILMS"
-      gon.product_id = 4
+      gon.products_id = 8
     elsif params[:code].present? && params[:code] == "6FILMS"
-      gon.product_id = 6
+      gon.products_id = 9
+    elsif params[:code].present? && params[:code] == "SVOD"
+      gon.products_id = 1
+    elsif params[:code].present? && params[:code] == "SVOD_ADULT"
+      gon.products_id = 5
+    elsif params[:moovie_id].present?
+      gon.products_id = params[:moovie_id]
     end
   end
 
