70c78ab1e0fb5eff847b5ae6ad2c1783f80c7453
Aleksandar Popovic <aleksandar.popovic@codepop.biz>
2017-04-02 11:30:00 +0200

    add phone number to sms codes

--- app/models/sms_code.rb
+++ app/models/sms_code.rb
@@ -1,3 +1,12 @@
 class SmsCode < ActiveRecord::Base
   attr_accessible :code
+
+  before_create :create_unique_code
+
+  def create_unique_code
+    begin
+      self.code = SecureRandom.hex(5)
+    end while self.class.exists?(:code => code)
+  end
+
 end
