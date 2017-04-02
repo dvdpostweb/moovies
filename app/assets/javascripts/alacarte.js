b6459eb632d3e82039e7e0e688242a83ccbd1129
Aleksandar Popovic <aleksandar.popovic@codepop.biz>
2017-04-02 20:41:33 +0200

    Set alacarte code packages

--- app/assets/javascripts/alacarte.js
+++ app/assets/javascripts/alacarte.js
@@ -35,7 +35,28 @@ $(document).ready(function () {
         var column = parseInt($(this).attr('data-box-order')) + 1;
         $('.cell').removeClass('selected');
         $('.row > .cell:nth-child(' + column + '), .headerRow > .cell:nth-child(' + column + ')').addClass('selected');
+        //console.log("clicked");
     });
 
+    $(".film_credits_btn_2").hide();
+    $(".film_credits_btn_4").hide();
+
+    $("#2_film_credits").click(function() {
+        $(".film_credits_btn_4").hide();
+        $(".film_credits_btn_6").hide();
+        $(".film_credits_btn_2").show();
+    });
+
+    $("#4_film_credits").click(function() {
+        $(".film_credits_btn_2").hide();
+        $(".film_credits_btn_6").hide();
+        $(".film_credits_btn_4").show();
+    });
+
+    $("#6_film_credits").click(function() {
+        $(".film_credits_btn_2").hide();
+        $(".film_credits_btn_4").hide();
+        $(".film_credits_btn_6").show();
+    });
 
 });
