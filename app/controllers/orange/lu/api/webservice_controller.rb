370d26137d927e4dfa23c87403e5f93d7948f792
Aleksandar Popovic <aleksandar.popovic@codepop.biz>
2017-04-02 12:39:46 +0200

    Create and save random sms codes

--- app/controllers/orange/lu/api/webservice_controller.rb
+++ app/controllers/orange/lu/api/webservice_controller.rb
@@ -4,12 +4,17 @@ class Orange::Lu::Api::WebserviceController < ApplicationController
   def is_eligable
     if request.xhr? && URI(request.referer).path == "/orange/lu/auth/#{I18n.locale}/sms/authorization" && params[:sms_number].present?
       customers_id = customer_signed_in? ? current_customer : 0
-      wcf_service_response = HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangeIseligable?customers_id=#{customers_id}&mobileNumber=#{params[:sms_number]}&SMSCodeMessage=%22dsgfdag%22&products_id=1758468")
-      if wcf_service_response.body = 0
-        render json: { status: 0, message: "Success message that SMS CODE IS SENT" }
-      elsif wcf_service_response.body = 1
-        render json: { status: 1, message: "Error" }
+      sms_code = SmsCode.new
+      sms_code.phone_number = params[:sms_number]
+      if sms_code.save!
+        render json: sms_code
       end
+      #wcf_service_response = HTTParty.get("https://www.plush.be:2355/WcfService/http/OrangeIseligable?customers_id=#{customers_id}&mobileNumber=#{params[:sms_number]}&SMSCodeMessage=%22dsgfdag%22&products_id=1758468")
+      #if wcf_service_response.body = 0 && SmsCode.create(sms_number: params[:sms_number])
+      #  render json: { status: 0, message: "Success message that SMS CODE IS SENT" }
+      #elsif wcf_service_response.body = 1
+      #  render json: { status: 1, message: "Error" }
+      #end
     else
       raise ActionController::RoutingError.new('Not Found')
     end
--- app/models/sms_code.rb
+++ app/models/sms_code.rb
@@ -1,11 +1,19 @@
 class SmsCode < ActiveRecord::Base
-  attr_accessible :code
+  attr_accessible :code, :phone_number
+
+  #validates :code, presence: true
+  validates :phone_number, presence: true
+  validates_uniqueness_of :code
 
   before_create :create_unique_code
 
+  def code=(val)
+    self[:code] = val.upcase
+  end
+
   def create_unique_code
     begin
-      self.code = SecureRandom.hex(5)
+      self.code = SecureRandom.hex(2)
     end while self.class.exists?(:code => code)
   end
 
