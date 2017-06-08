ed8dd9da01ff8a9877fecfad5d3326cb2a3c2cc5
YOUR NAME <YOUR@EMAIL.com>
2017-06-08 16:42:57 +0200

    Subscription page does not working, when subscriber is LogedIn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,5 @@
 Moovies::Application.configure do
 
-  config.react.variant = :development
-
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
