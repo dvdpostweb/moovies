0c955818211ed2d456cf37f483a8b145dfa625e7
popac <YOUR@EMAIL.com>
2017-06-15 11:04:57 +0200

    Upgrade to ruby 2.3.4p301

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :deploy_user, 'plush'
 set :repo_url, "git@github.com:dvdpost/moovies.git"
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.0.0-p648'
+set :rbenv_ruby, '2.3.4p301'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
