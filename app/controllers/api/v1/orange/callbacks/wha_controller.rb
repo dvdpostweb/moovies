511a973fa0ad275e8c228cf0416614ce35007e3a
popac <YOUR@EMAIL.com>
2017-06-16 19:18:06 +0200

    WHA V3

--- app/controllers/api/v1/orange/callbacks/wha_controller.rb
+++ app/controllers/api/v1/orange/callbacks/wha_controller.rb
@@ -2,9 +2,12 @@ class Api::V1::Orange::Callbacks::WhaController < ApplicationController
 
   def success
     #render json: params
-    if params[:mp].present?
-      render json: params[:mp]
-    end
+    #if params[:mp].present?
+    #  render json: params[:mp]
+    #end
+
+    params.each { |p| render json: p }
+
   end
 
   def cancel
