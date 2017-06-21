0c6e187c70e4cb8402dcae418fc056e398aec2b1
popac <YOUR@EMAIL.com>
2017-06-21 14:48:18 +0200

    origins fix

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Moovies
   class Application < Rails::Application
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins 'http://staging.plush.lu'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
