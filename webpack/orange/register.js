64c238482ec13ff87672b5fd21d15b131ee57e33
popac <YOUR@EMAIL.com>
2017-06-26 19:10:46 +0200

    OPR

--- webpack/orange/register.js
+++ webpack/orange/register.js
@@ -1,4 +1,6 @@
-$("#sms_number_register").val(localStorage.getItem("plush_phone_number").slice(4));
+if (localStorage.getItem("plush_phone_number") !== null) {
+    $("#sms_number_register").val(localStorage.getItem("plush_phone_number").slice(4));
+}
 jQuery.validator.addMethod("phone", function (phone_number, element) {
     phone_number = phone_number.replace(/\s+/g, "");
     return this.optional(element) || phone_number.length > 8 &&
@@ -68,6 +70,83 @@ $("#is_eligable_registration").validate({
     }
 });
 
+$("#orange_purchase_register").validate({
+    rules: {
+        "sms-code": {
+            required: true,
+            remote: {
+                url: "/orange/lu/api/check_sms_activation_code"
+            }
+        }
+    },
+    messages: {
+        "sms-code": {
+            required: orangePurchaseMessage(),
+            remote: orangePurchaseMessageSmsCodeValidation()
+        }
+    },
+    highlight: function (element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    submitHandler: function (form) {
+        sms_number = localStorage.getItem("plush_phone_number");
+        $.ajax({
+            method: 'POST',
+            url: '/orange/lu/api/orange_purchase',
+            data: {
+                'sms_code': $.trim($("#sms_code").val()),
+                'products_id': gon.products_id,
+                'plush_phone_number': sms_number.slice(1),
+                'code': gon.code
+            },
+            dataType: 'json',
+            success: function (response) {
+
+                if ("TRUE" === response.status) {
+
+
+                    $.ajax({
+                        method: 'POST',
+                        url: '/orange/lu/api/automatic_register',
+                        data: {
+                            'plush_phone_number': sms_number.slice(1),
+                            'products_id': gon.products_id,
+                        },
+                        dataType: 'json',
+                        success: function (response) {
+                            if (0 === response.status) {
+                                window.location.href = response.redirect_path
+                            }
+                        },
+                        error: function (response) {
+                            jQuery.facebox("<div class=\"alert alert-danger\">" +
+                                "<strong>" + response.status + "</strong>" +
+                                "</div>");
+                        }
+                    });
+
+
+                } else {
+                    jQuery.facebox("<div class=\"alert alert-danger\">" +
+                        "<strong>" + response.status + "</strong>" +
+                        "</div>");
+                }
+
+            },
+            error: function (response) {
+                jQuery.facebox("<div class=\"alert alert-danger\">" +
+                    "<strong>" + response.status + "</strong>" +
+                    "</div>");
+            }
+        });
+    }
+});
+
 function isEligableValidationMessage() {
     if (gon.locale == "fr") {
         "Numéro de téléphone est nécessaire.";
@@ -75,5 +154,25 @@ function isEligableValidationMessage() {
         "Telefoonnummer is vereist.";
     } else if (gon.locale == "en") {
         "Phone number is required.";
+    }
+}
+
+function orangePurchaseMessage() {
+    if (gon.locale == "fr") {
+        "SMS code est nécessaire."
+    } else if (gon.locale == "nl") {
+        "SMS code is vereist."
+    } else if (gon.locale == "en") {
+        "SMS code is required."
+    }
+}
+
+function orangePurchaseMessageSmsCodeValidation() {
+    if (gon.locale == "fr") {
+        "Le code SMS n'est pas correct."
+    } else if (gon.locale == "nl") {
+        "SMS code is niet juist."
+    } else if (gon.locale == "en") {
+        "SMS code is not correct."
     }
 }
