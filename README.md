8b385a07b3c2e40146322b6afe3da7ad2a418b32
popac <YOUR@EMAIL.com>
2017-06-26 23:34:54 +0200

    convert_to_customer

--- README.md
+++ README.md
@@ -4,11 +4,8 @@
 
 ### cap staging deploy && cap staging invoke:rake TASK=webpack:compile
 
-The field of classical Artificial Intelligence (AI) coalesced in the 1950s
-drawing on an understanding of the brain from neuroscience, the new
-mathematics of information theory, control theory referred to as cyber-
-netics, and the dawn of the digital computer. AI is a cross-disciplinary
-field of research that is generally concerned with developing and in-
-vestigating systems that operate or act intelligently. It is considered
-a discipline in the field of computer science given the strong focus on
-computation.
+## PRODUCTION DEPLOY <br>
+
+### cap production deploy && cap production invoke:rake TASK=webpack:compile
+
+
---
+++ app/assets/javascripts/components/promoimage.jsx
@@ -0,0 +1,19 @@
+window.Promoimage = React.createClass({
+    render: function() {
+        var image =  {
+            "background-image": 'url('+ this.props.image + ')'
+        }
+        return (
+            <div className="container white-bck">
+                <div href={this.props.image} className="promo-image rwd-bck display-block" style={image}  >
+                    <div className="promo-image-overlay">
+                        <div className="promo-image-overlay-inner white bold">
+                            <span>Vivez avec nous les plus grands moments du cinéma</span>
+                            <button>Découvrez nos formules</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
