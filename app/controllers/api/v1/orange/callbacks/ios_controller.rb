4ace8a8b7deeb18265e29f8ee221e6447d4f9587
popac <YOUR@EMAIL.com>
2017-07-03 14:07:40 +0200

    Set IOS movie token

--- app/controllers/api/v1/orange/callbacks/ios_controller.rb
+++ app/controllers/api/v1/orange/callbacks/ios_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Orange::Callbacks::IosController < API::V1::BaseController
 
   def orangemobile
     if params[:imdb_id].present?
-      sp_chromecast_data = ActiveRecord::Base.connection.exec_query %Q{CALL sp_chromecast_data(#{params[:imdb_id]})}
+      sp_chromecast_data = ActiveRecord::Base.connection.exec_query %Q{CALL sp_chromecast_data(#{params[:imdb_id]}, #{params[:cn]})}
       render json: sp_chromecast_data
     end
   end
--- webpack/streaming_product/streaming.js
+++ webpack/streaming_product/streaming.js
@@ -60,28 +60,17 @@ $(document).ready(function () {
             method: 'POST',
             url: '/api/v1/orange/callbacks/orangemobile',
             data: {
-                'imdb_id': gon.imdb_id
+                'imdb_id': gon.imdb_id,
+                'cn': gon.current_customer.customers_id
             },
             dataType: 'json',
             success: function (response) {
-
                 var callbackUrl = encodeURIComponent("http://staging.plush.be/api/v1/orange/callbacks/ioscallback?cn="+gon.current_customer.customers_id+"&products_id="+pid+"");
                 var audiosubs = encodeURIComponent(response[0]["audsub"]);
                 var poster = encodeURIComponent(response[0]["products_image_big"]);
                 var title = encodeURIComponent(gon.products_title);
-
                 url = "plush://play?cn="+gon.current_customer.customers_id+"&imdb_id="+product+"&disk_id=0&season_id=0&audiosubs="+audiosubs+"&poster="+poster+"&name="+title+"&callback="+callbackUrl+"";
-
                 window.location = url;
-
-                //console.log("http://staging.plush.be/api/v1/orange/callbacks/ioscallback?cn="+gon.current_customer.customers_id+"&products_id="+pid+"")
-
-                //$(".qualityvod").last().trigger("click");
-
-                //setTimeout(function() {
-                //    jwplayer('player').stop();
-                //}, 1000);
-
             },
             error: function (response) {
                 jQuery.facebox("<div class=\"alert alert-danger\">" +
@@ -89,18 +78,6 @@ $(document).ready(function () {
                     "</div>");
             }
         });
-
-        //var userAgent = window.navigator.userAgent;
-        //if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
-        //    url = "plush://play?cn="+gon.current_customer.customers_id+"&imdb_id="+product+"&disk_id=0&season_id=0&callback=http://staging.plush.be/api/v1/orange/callbacks/orangemobile?cn="+gon.current_customer.customers_id+"&products_id="+pid+"";
-        //    encodedUrl = encodeURIComponent(url);
-        //    window.location = encodedUrl;
-        //    console.log(encodedUrl);
-        //}
-        //$(".qualityvod").last().trigger("click");
-        //setTimeout(function() {
-        //    jwplayer('player').stop();
-        //}, 1000);
     });
 
 
