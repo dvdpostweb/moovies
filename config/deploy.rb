99b4c4b48d8c2c71e58a0d8bef60bf733c3c7d46
Author Name <author.name@mail.com>
2017-07-04 14:31:12 +0200

    Set WHA

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require 'thinking_sphinx/capistrano'
 require './config/boot'
 
 set :stages, %w(staging production)
-set :default_stage, "production"
+set :default_stage, "staging"
 set :whenever_command, "bundle exec whenever"
 
 
