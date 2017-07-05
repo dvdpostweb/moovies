220bad2120b1be466874d02fd2eeb9ab3c2171e9
popac <mozgovstina@gmail.com>
2017-07-05 18:25:27 +0200

    WHA && SMS CONFIRM DIFERENCES

--- app/controllers/api/v1/orange/callbacks/wha_controller.rb
+++ app/controllers/api/v1/orange/callbacks/wha_controller.rb
@@ -81,16 +81,18 @@ class Api::V1::Orange::Callbacks::WhaController < ApplicationController
         end
       end
     elsif 'v=3:{c=PurchaseTypeCancel'.in?(niz)
-      WhaCallback.create(created_at: Time.now, callbackurl: params)
+      render json: params
+      #WhaCallback.create(created_at: Time.now, callbackurl: params)
       #flash[:error] = t('orange_wha_cancel')
       #redirect_to root_localize_path and return
     end
   end
 
   def cancel
-    WhaCallback.create(created_at: Time.now, callbackurl: params)
-    flash[:error] = t('orange_wha_cancel')
-    redirect_to root_localize_path and return
+    render json: params
+    #WhaCallback.create(created_at: Time.now, callbackurl: params)
+    #flash[:error] = t('orange_wha_cancel')
+    #redirect_to root_localize_path and return
   end
 
 end
