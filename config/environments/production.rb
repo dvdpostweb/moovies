67bfb839db7bd03885ef8ad726f1289838c5f8c0
popac <mozgovstina@gmail.com>
2017-07-05 15:51:34 +0200

    Fix contact html

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,11 +4,11 @@ Moovies::Application.configure do
   #config.action_controller.asset_host = "http://www.plush.be"
 
   # Code is not reloaded between requests
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
