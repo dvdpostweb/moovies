bb04c8233a0c486acdef572a63f5c984123fd5ae
popac <mozgovstina@gmail.com>
2017-07-07 16:34:06 +0200

    Set up Wister Auth with jwt token

--- app/controllers/wister/auth_controller.rb
+++ app/controllers/wister/auth_controller.rb
@@ -1,7 +1,18 @@
+require 'jwt'
+
 class Wister::AuthController < API::V1::BaseController
 
   def jwt
-    render json: params
+    if params[:tokenwister].present?
+      decoded_token = JWT.decode params[:tokenwister], nil, false, { :algorithm => 'HS256' }
+      decoded_token.split(/,/).to_a
+      email = decoded_token[0]["email"]
+      customer = Customer.find_by_email(email)
+      if customer.present?
+        sign_in(customer)
+        redirect_to root_localize_path
+      end
+    end
   end
 
 end
