7bc6f85d6eadf446ca140105d3316fd539a84b51
Aleksandar Popovic <aleksandar.popovic@hotmail.com>
2017-07-11 17:10:49 +0200

    St player

--- app/helpers/streaming_products_helper.rb
+++ app/helpers/streaming_products_helper.rb
@@ -20,22 +20,14 @@ module StreamingProductsHelper
     javascript_tag script
   end
 
-  def jwplayerdefault(source_file, source, streaming, token_name, browser, code = nil, season_id ='0', episode_id = '0', image)
+  def jwplayerdefault(source_file, source, streaming, token_name, browser, code = nil, season_id ='0', episode_id = '0')
     audio = streaming.languages.by_language(:fr).first.short_alpha
     sub = streaming.subtitles.count > 0 ? streaming.subtitles.by_language(:fr).first.short_alpha : 'non'
     hd = streaming.hd? ? true : false
     url = Moovies.akamai_hls_url(streaming.imdb_id, audio, sub, hd, streaming.videoland, streaming.akamai_folder, season_id , episode_id)
 
     script = <<-script
-      var movieUrl = '#{url}';
-      productImage = '#{image}';
-
-      jwplayer('player_default').setup({
-        file: movieUrl,
-        autostart: false,
-        controls: false,
-        image: productImage
-      });
+      var movieUrlDefault = '#{url}';
     script
     javascript_tag script
   end
