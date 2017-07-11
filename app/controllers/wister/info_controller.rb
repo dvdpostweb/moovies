1c4e970acc2e5f5f1abe8d247d689eb132733e4f
Aleksandar Popovic <aleksandar.popovic@hotmail.com>
2017-07-11 18:19:40 +0200

    wister task 2 done

---
+++ app/controllers/wister/info_controller.rb
@@ -0,0 +1,7 @@
+class Wister::InfoController < API::V1::BaseController
+
+  def conditions
+
+  end
+
+end
--- config/routes/wister.rb
+++ config/routes/wister.rb
@@ -2,5 +2,8 @@ Moovies::Application.routes.draw do
   namespace :wister do
     get "auth/jwt"
     get "auth/token_creator"
+    scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
+      get "info/conditions"
+    end
   end
 end
