a08bc3208b4cdde4f3607ce73c235e06ee078524
Aleksandar Popovic <aleksandar.popovic@hotmail.com>
2017-07-11 04:20:20 +0200

    Set npm

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,10 @@ require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 require 'thinking_sphinx/capistrano'
 require './config/boot'
+require 'capistrano/npm'
 
 set :stages, %w(staging production)
-set :default_stage, "production"
+set :default_stage, "staging"
 set :whenever_command, "bundle exec whenever"
 
 
